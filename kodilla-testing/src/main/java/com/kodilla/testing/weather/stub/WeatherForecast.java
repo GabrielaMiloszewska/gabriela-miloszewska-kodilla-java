85deccdfeba48d322bfc25f61784cdb33dfb8fe5
Gabriela Miłoszewska <gabriela_miloszewska@wp.pl>
2021-05-26 15:28:39 +0200

    6.5 zadanie rozbudowa WeatherForecastTestSuite

--- kodilla-testing/src/main/java/com/kodilla/testing/weather/stub/WeatherForecast.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/weather/stub/WeatherForecast.java
@@ -1,7 +1,6 @@
 package com.kodilla.testing.weather.stub;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class WeatherForecast {
     private Temperatures temperatures;
@@ -20,5 +19,29 @@ public class WeatherForecast {
             resultMap.put(temperature.getKey(), temperature.getValue() + 1.0); // [1]
         }
         return resultMap;
+    }
+
+    public double calculateAverageTemperature () {
+        double averageTemperature = 0;
+        double mapSize = temperatures.getTemperatures().size();
+        for (Map.Entry<String, Double> temperature : temperatures.getTemperatures().entrySet()) {
+            averageTemperature += temperature.getValue();
+        }
+        return averageTemperature / mapSize;
+    }
+
+    public double calculateMedian() {
+        List<Double> series = new ArrayList<>();
+        for (Map.Entry<String, Double> temperature : temperatures.getTemperatures().entrySet()) {
+            series.add(temperature.getValue());
+        }
+        double median = 0;
+        Collections.sort(series);
+        if (series.size() % 2 == 0) {
+            median = series.get((series.size() - 1) / 2);
+        } else {
+            median = (series.get((series.size() - 1) / 2) + series.get(series.size() / 2)) / 2;
+        }
+        return median;
     }
 }
