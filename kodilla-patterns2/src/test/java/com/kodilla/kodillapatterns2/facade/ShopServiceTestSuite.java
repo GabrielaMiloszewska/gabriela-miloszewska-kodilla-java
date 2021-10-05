9f38e9cf79e801419e4913d88eeeff11700acc35
Gabriela Miłoszewska <gabriela_miloszewska@wp.pl>
2021-10-05 15:24:57 +0200

    20.3. Programowanie aspektowe w Spring Boot

--- kodilla-patterns2/src/test/java/com/kodilla/kodillapatterns2/facade/ShopServiceTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/kodillapatterns2/facade/ShopServiceTestSuite.java
@@ -5,12 +5,15 @@ import com.kodilla.kodillapatterns2.facade.api.OrderDto;
 import com.kodilla.kodillapatterns2.facade.api.OrderFacade;
 import com.kodilla.kodillapatterns2.facade.api.OrderProcessingException;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.math.BigDecimal;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class ShopServiceTestSuite {
 
     @Autowired
